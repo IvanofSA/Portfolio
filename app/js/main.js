3b5bb8fee7993aab1dd43cef4d086f5fcb51c21c
Ivanov Sergey <poshelya@gmail.com>
2015-10-05 00:25:59 +0300

    page2 mywork

---
+++ app/js/main.js
@@ -0,0 +1,26 @@
+"use strict"
+
+var MYAPP = function(){
+   var callPopup = $('#add'),
+       popup = $('#popup');
+
+   callPopup.on('click', function(e){
+       e.preventDefault();
+       popup.bPopup();
+   })
+};
+MYAPP();
+
+function getName (str){
+    if (str.lastIndexOf('\\')){
+        var i = str.lastIndexOf('\\')+1;
+    }
+    else{
+        var i = str.lastIndexOf('/')+1;
+    }						
+    var filename = str.slice(i);			
+    var uploaded = document.getElementById("fileformlabel");
+    uploaded.innerHTML = filename;
+}
+
+
